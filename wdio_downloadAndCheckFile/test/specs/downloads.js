8a9ce7a056d62ee84fdffca16c513f2ffebb5e06
Emma Hawkins <emma.hawkins@sky.uk>
2018-11-20 09:50:06 +0000

    Added test to Download and check file contents.

---
+++ wdio_downloadAndCheckFile/test/specs/downloads.js
@@ -0,0 +1,26 @@
+const { URL } = require('url');
+const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
+const checkExistsWithTimeout = require('../../utils/checkIfExistsWithTimeout');
+
+describe('Downloads', () => {
+    it('should download the file', () => {
+        browser.url('./download');
+        const downloadLink = $('*=some-file.txt');
+        downloadLink.click();
+        const downloadHref = downloadLink.getAttribute('href');
+        const downloadUrl = new URL(downloadHref);
+        const fullPath = downloadUrl.pathname;
+        const splitPath = fullPath.split('/');
+        const fileName = splitPath.splice(-1)[0];
+        const filePath = path.join(global.downloadDir, fileName);
+        browser.call( function () {
+            return checkExistsWithTimeout(filePath, 60000);
+        });
+        const fileContents = fs.readFileSync(filePath, 'utf-8');
+        console.log('File contents are: ', fileContents);
+        
+        assert.ok(fileContents.includes('asdf'));
+    });
+});
